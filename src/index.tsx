eb6d9cf0f5eae52131da0a1c835fef07350d57e7
xThePOD <xthepodx@gmail.com>
2024-08-14 20:46:10 -0500

    letsee

---
+++ src/index.tsx
@@ -0,0 +1,200 @@
+import { serveStatic } from '@hono/node-server/serve-static'
+import { Button, Frog, TextInput } from 'frog'
+import { devtools } from 'frog/dev'
+//import { neynar } from 'frog/hubs'
+
+export const app = new Frog({
+  title: 'first frame',
+  //hub: neynar({ apiKey: '000000-87D9-00008-95A2-000007C12421' }),
+});
+
+app.use('/*', serveStatic({ root: './public' }))
+
+app.frame('/', (c) => {
+  const { buttonValue, inputText, status } = c
+  const fruit = inputText || buttonValue
+  return c.res({
+    action: '/secondframe',
+    image: (
+      <div
+        style={{
+          alignItems: 'center',
+          background:
+            status === 'response'
+              ? 'linear-gradient(to right, #432889, #17101F)'
+              : 'black',
+          backgroundSize: '100% 100%',
+          display: 'flex',
+          flexDirection: 'column',
+          flexWrap: 'nowrap',
+          height: '100%',
+          justifyContent: 'center',
+          textAlign: 'center',
+          width: '100%',
+        }}
+      >
+        <div
+          style={{
+            color: 'white',
+            fontSize: 60,
+            fontStyle: 'normal',
+            letterSpacing: '-0.025em',
+            lineHeight: 1.4,
+            marginTop: 30,
+            padding: '0 120px',
+            whiteSpace: 'pre-wrap',
+          }}
+        >
+          {status === 'response'
+            ? `Nice choice.${fruit ? ` ${fruit.toUpperCase()}!!` : ''}`
+            : 'Welcome!'}
+        </div>
+      </div>
+    ),
+    intents: [
+      <Button>Enter</Button>,
+    ],
+  });
+});
+
+app.frame('/secondframe', (c) => {
+  const { buttonValue, inputText, status } = c
+  const fruit = inputText || buttonValue
+  return c.res({
+    action: '/thirdframe',
+    image: (
+      <div
+        style={{
+          alignItems: 'center',
+          background:
+            status === 'response'
+              ? 'linear-gradient(to right, #432889, #17101F)'
+              : 'black',
+          backgroundSize: '100% 100%',
+          display: 'flex',
+          flexDirection: 'column',
+          flexWrap: 'nowrap',
+          height: '100%',
+          justifyContent: 'center',
+          textAlign: 'center',
+          width: '100%',
+        }}
+      >
+        <div
+          style={{
+            color: 'white',
+            fontSize: 60,
+            fontStyle: 'normal',
+            letterSpacing: '-0.025em',
+            lineHeight: 1.4,
+            marginTop: 30,
+            padding: '0 120px',
+            whiteSpace: 'pre-wrap',
+          }}
+        >
+          The POD 🐳
+
+        </div>
+      </div>
+    ),
+    intents: [
+      <Button.Reset>Back</Button.Reset>,
+      <Button>Next</Button>,
+    ],
+  })
+})
+
+app.frame('/thirdframe', (c) => {
+  const { buttonValue, inputText, status } = c
+  const fruit = inputText || buttonValue
+  return c.res({
+    image: (
+      <div
+        style={{
+          alignItems: 'center',
+          background:
+            status === 'response'
+              ? 'linear-gradient(to right, #432889, #17101F)'
+              : 'black',
+          backgroundSize: '100% 100%',
+          display: 'flex',
+          flexDirection: 'column',
+          flexWrap: 'nowrap',
+          height: '100%',
+          justifyContent: 'center',
+          textAlign: 'center',
+          width: '100%',
+        }}
+      >
+        <div
+          style={{
+            color: 'white',
+            fontSize: 60,
+            fontStyle: 'normal',
+            letterSpacing: '-0.025em',
+            lineHeight: 1.4,
+            marginTop: 30,
+            padding: '0 120px',
+            whiteSpace: 'pre-wrap',
+          }}
+        >
+          Introduction!
+
+        </div>
+      </div>
+    ),
+    intents: [
+      <Button action='/secondframe'>Back</Button>,
+      <Button action='/fourthframe'>Next</Button>,
+    ],
+  })
+})
+
+// app.frame('/fourthframe', (c) => {
+//   const { buttonValue, inputText, status } = c
+//   const fruit = inputText || buttonValue
+//   return c.res({
+//     action:'/thirdframe',
+//     image: (
+//       <div
+//         style={{
+//           alignItems: 'center',
+//           background:
+//             status === 'response'
+//               ? 'linear-gradient(to right, #432889, #17101F)'
+//               : 'black',
+//           backgroundSize: '100% 100%',
+//           display: 'flex',
+//           flexDirection: 'column',
+//           flexWrap: 'nowrap',
+//           height: '100%',
+//           justifyContent: 'center',
+//           textAlign: 'center',
+//           width: '100%',
+//         }}
+//       >
+//         <div
+//           style={{
+//             color: 'white',
+//             fontSize: 60,
+//             fontStyle: 'normal',
+//             letterSpacing: '-0.025em',
+//             lineHeight: 1.4,
+//             marginTop: 30,
+//             padding: '0 120px',
+//             whiteSpace: 'pre-wrap',
+//           }}
+//         >
+//           Introduction!
+
+//         </div>
+//       </div>
+//     ),
+//     intents: [
+//       <Button>Back</Button>,
+//       <Button value="next">Next</Button>,
+//     ],
+//   })
+// })
+
+devtools(app, { serveStatic })
